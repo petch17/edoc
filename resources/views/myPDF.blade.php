ba867479e8f673e11ca8483a5daa6d0fc18b2f70
NAME <psp_phet@hotmail.com>
2019-08-26 14:08:20 +0700

    up

--- resources/views/myPDF.blade.php
+++ resources/views/myPDF.blade.php
@@ -1,1 +1,20 @@
-{{ $booknum }}
+<html>
+    <head>
+
+        <style>
+            @font-face{
+                font-family: 'THSarabunNew';
+                font-style: normal;
+                font-weight: normal;
+                src: url("{{ asset('font/THSarabunNew.ttf') }}") format('truetype');
+            }
+
+        </style>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+        <title> ใบปฏิบัติการ </title>
+    </head>
+    <body style="font-family: 'THSarabunNew'; ">
+        <p> {{ $name }} </p>
+    </body>
+</html>
